b70507ab939ce589474032cd17b547a65c2f8588
Maher Attouche <maherattouche7@gmail.com>
2020-03-30 21:20:39 +0200

    calculatriceRpn qui represente le main

---
+++ src/main/java/uvsq/pglpp42/CalculatriceRpn.java
@@ -0,0 +1,16 @@
+package uvsq.pglpp42;
+
+/**
+ * Hello world!.
+ *
+ */
+public class CalculatriceRpn {
+  /**
+   * methode qui démare le programme en appelant saisieRpn.
+   * @param args sert
+   */
+  public static void main(String[] args) {
+    SaisieRpn rr = new SaisieRpn();
+    rr.saisir();
+  }
+}
